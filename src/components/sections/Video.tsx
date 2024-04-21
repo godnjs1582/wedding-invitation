0c13f0bd62702f3b86b70332d1078b697821d2e3
Kwon HaeWon <qpinky12@naver.com>
2024-04-21 16:47:04 +0900

    feat:video 추가

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -5,7 +5,13 @@ import Section from '@shared/Section'
 const cx = classNames.bind(styles)
 
 function Video() {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video autoPlay={true} muted={true} loop poster="/assets/poster.png">
+        <source src="/assets/main.mp4" type="video/mp4"></source>
+      </video>
+    </Section>
+  )
 }
 
 export default Video
